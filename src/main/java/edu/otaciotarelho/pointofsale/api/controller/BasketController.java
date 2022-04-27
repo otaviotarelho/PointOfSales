1a42aa6f4d3eb235967ed501544f82dc70f8ac5f
otavio.tarelho <otavio.tarelho@ifood.com.br>
2022-04-27 18:05:39 -0300

    exception handler

--- src/main/java/edu/otaciotarelho/pointofsale/api/controller/BasketController.java
+++ src/main/java/edu/otaciotarelho/pointofsale/api/controller/BasketController.java
@@ -3,19 +3,26 @@ package edu.otaciotarelho.pointofsale.api.controller;
 import edu.otaciotarelho.pointofsale.domain.business.BasketService;
 import edu.otaciotarelho.pointofsale.domain.checkout.Basket;
 import edu.otaciotarelho.pointofsale.domain.checkout.dto.BasketDTO;
+import edu.otaciotarelho.pointofsale.domain.exception.BasketNotFoundException;
+import edu.otaciotarelho.pointofsale.domain.exception.EmptyBasketException;
+import edu.otaciotarelho.pointofsale.domain.exception.ProductNotFoundException;
+import edu.otaciotarelho.pointofsale.domain.exception.StandardError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.UUID;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.ResponseEntity.created;
 
 @RestController
--- src/main/java/edu/otaciotarelho/pointofsale/api/handler/ControllerHandler.java
+++ src/main/java/edu/otaciotarelho/pointofsale/api/handler/ControllerHandler.java
@@ -7,48 +7,26 @@ import edu.otaciotarelho.pointofsale.domain.exception.StandardError;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @ControllerAdvice
-public class ControllerHandler {
+public class ControllerHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(BasketNotFoundException.class)
-    public ResponseEntity<StandardError> notFoundBasket(BasketNotFoundException e, HttpServletRequest request){
+    @ExceptionHandler({
+            BasketNotFoundException.class,
+            ProductNotFoundException.class,
+            EmptyBasketException.class
+    })
+    public ResponseEntity<StandardError> handleException(BasketNotFoundException e, WebRequest request){
         var err  = StandardError.builder()
                 .timestamp(System.currentTimeMillis())
                 .status(NOT_FOUND.value())
-                .error("Basket Not Found")
+                .error("Error occurred processing request")
                 .message(e.getMessage())
-                .path(request.getRequestURI())
-                .build();
-
-        return ResponseEntity.status(NOT_FOUND).body(err);
-    }
-
-    @ExceptionHandler(ProductNotFoundException.class)
-    public ResponseEntity<StandardError> notFoundProduct(ProductNotFoundException e, HttpServletRequest request){
-        var err  = StandardError.builder()
-                .timestamp(System.currentTimeMillis())
-                .status(NOT_FOUND.value())
-                .error("Product Not Found")
-                .message(e.getMessage())
-                .path(request.getRequestURI())
-                .build();
-
-        return ResponseEntity.status(NOT_FOUND).body(err);
-    }
-
-    @ExceptionHandler(EmptyBasketException.class)
-    public ResponseEntity<StandardError> notFoundProduct(EmptyBasketException e, HttpServletRequest request){
-        var err  = StandardError.builder()
-                .timestamp(System.currentTimeMillis())
-                .status(NOT_FOUND.value())
-                .error("Empty Basket")
-                .message(e.getMessage())
-                .path(request.getRequestURI())
+                .path(request.getContextPath())
                 .build();
 
         return ResponseEntity.status(NOT_FOUND).body(err);
